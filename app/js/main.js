91024ec901ac92d6efcbd6c55a7dde6a80dc60dc
olol1631 <olol1631@google.com>
2021-03-24 00:27:12 +0300

    add news

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,26 @@
 $(document).ready(function(){
-    $('.menu__burger').click(function(event){
-        $('.menu__burger,.menu__list').toggleClass('active');
+    //свернуть меню
+    $('.menu-burger').click(function(event){
+        $('.menu-burger,.menu__list').toggleClass('active');
         $('body').toggleClass('lock');
     });
-    //спойлеры
+
+    //изменить вид карточек товаров
+    $('#line').click(function(event){
+        $('.product__item').addClass('line');
+        $('.icon-table').removeClass('active');
+        $('.icon-line').addClass('active');
+    });
+    $('#table').click(function(event){
+      $('.product__item').removeClass('line');
+      $('.icon-line').removeClass('active');
+      $('.icon-table').addClass('active');
+    });
+
+    //свернуть фильтр 
     $('.product__filter-title').click(function(event){
         $(this).toggleClass('active').next().slideToggle(300);
-    })
-
+    });    
 
     //slider
     new Swiper('.banner-container', {
@@ -21,54 +34,52 @@ $(document).ready(function(){
           el: '.swiper-pagination',
         },
       });
-      new Swiper('.brands-container', {
-        slidesPerView: 3,
-        spaceBetween: 10,
-        freeMode: true,
-        loop: true,
-        autoplay: {
-            delay: 5000,
-            disableOnInteraction: false,
-          },
-        breakpoints: {
-          640:{
-            slidesPerView: 4,
-          },
-          760: {
-            slidesPerView: 5,
-            spaceBetween: 30,
-          },
-          992: {
-            slidesPerView: 6,
-          },
-        }
-          
-      });
-      new Swiper('.new-container', {
-        slidesPerView: 2,
-        spaceBetween: 15,
-        loop: true,
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+    new Swiper('.brands-container', {
+      slidesPerView: 3,
+      spaceBetween: 10,
+      freeMode: true,
+      loop: true,
+      autoplay: {
+          delay: 5000,
+          disableOnInteraction: false,
         },
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
+      breakpoints: {
+        640:{
+          slidesPerView: 4,
         },
-        breakpoints: {
-          640:{
-            slidesPerView: 3,
-          },
-          768:{
-            slidesPerView: 4,
-          },
-          992: {
-            slidesPerView: 5,
-          },
-        }
-      });
-
-      
+        760: {
+          slidesPerView: 5,
+          spaceBetween: 30,
+        },
+        992: {
+          slidesPerView: 6,
+        },
+      }
+        
+    });
+    new Swiper('.new-container', {
+      slidesPerView: 2,
+      spaceBetween: 15,
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      breakpoints: {
+        640:{
+          slidesPerView: 3,
+        },
+        768:{
+          slidesPerView: 4,
+        },
+        992: {
+          slidesPerView: 5,
+        },
+      }
+    });      
       
     });
